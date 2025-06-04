968e70c6674f6f2da057ca8e0224849f8a78a2e2
alcanary <ahmadqowi03@gmail.com>
2025-06-05 04:13:10 +0800

    Jpa Manager,3rd migration, more testing unit

--- src/test/java/io/github/jayennn/BlockchainVoting/utils/JpaManagerTest.java
+++ src/test/java/io/github/jayennn/BlockchainVoting/utils/JpaManagerTest.java
@@ -1,27 +1,22 @@
-package io.github.jayennn.BlockchainVoting.database;
+package io.github.jayennn.BlockchainVoting.utils;
 
 import io.github.jayennn.BlockchainVoting.entity.Gender;
 import io.github.jayennn.BlockchainVoting.entity.Role;
 import io.github.jayennn.BlockchainVoting.entity.User;
 import io.github.jayennn.BlockchainVoting.entity.Voter;
-import io.github.jayennn.BlockchainVoting.utils.ConfigManager;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 
 
 import java.time.LocalDate;
 import java.util.Properties;
 
-public class DatabaseTest {
+public class JpaManagerTest {
     @Test
     public void initiateData(){
-        Properties properties = ConfigManager.getInstance().getProperties();
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("main-pu",properties);
-
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = JpaManager.getInstance().getEM();
 
         em.getTransaction().begin();
 
@@ -30,14 +25,36 @@ public class DatabaseTest {
         voter_0.setName("Rojo Kasino");
         voter_0.setGender(Gender.MALE);
         voter_0.setDateOfBirth(LocalDate.of(2006,8,31));
-
+        em.persist(voter_0);
 
         User user_1 = new User(Role.USER,"bukabuka");
         user_1.setVoter(voter_0);
         user_1.setUsername("rojo-soprano");
-        user_1.setPassword("testtest");
+
+        UpdatableBcrypt UBcrypt = new UpdatableBcrypt(12);
+        user_1.setPassword(UBcrypt.hash("1234abcd"));
         em.persist(user_1);
+
         em.getTransaction().commit();
+
+    }
+
+    @Test
+    public void comparePassword(){
+        EntityManager em = JpaManager.getInstance().getEM();
+
+        em.getTransaction().begin();
+
+        User user = em.createQuery(
+                "SELECT u FROM User u WHERE u.username = :username", User.class)
+                .setParameter("username","rojo-soprano")
+                .getSingleResult();
+
+        String password = "1234abcd";
+        UpdatableBcrypt UBcrypt = new UpdatableBcrypt(12);
+        Boolean result = UBcrypt.verifyHash(password,user.getPassword());
+
+        System.out.println(result);
 
     }
 
