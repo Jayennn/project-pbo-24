233d0ce3af1ee7aef824a1f4cac0b5c79442d322
alcanary <ahmadqowi03@gmail.com>
2025-06-12 01:02:24 +0800

    delete logs

--- src/main/java/io/github/jayennn/BlockchainVoting/entity/Vote.java
+++ src/main/java/io/github/jayennn/BlockchainVoting/entity/Vote.java
@@ -3,10 +3,14 @@ package io.github.jayennn.BlockchainVoting.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "votes")
 public class Vote {
   @Id
-  @Column(length = 10)
-  private String id;
+  @Column(columnDefinition = "binary(16)")
+  private String election_id;
+
+
 }
